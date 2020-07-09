1256bc09b55ddc863d7443037b365dff08680542
Владислав Ковальчук <dobro322@yandex.ru>
2020-07-09 21:40:26 +0000

    Update api_v1/main/endpoints/menu/inheritances_api.py

--- api_v1/main/endpoints/menu/inheritances_api.py
+++ api_v1/main/endpoints/menu/inheritances_api.py
@@ -1,9 +1,10 @@
 from flask import request, abort
 from flask_restx import Namespace, Resource, fields
-
+from flask_sqlalchemy import or_, and_
 from api_v1.main import db
 from api_v1.main.endpoints.menu.menu import Inheritances
 from api_v1.main.utils import split_dict_args
+from 
 
 api = Namespace('inheritances_api', description='Inheritances API')
 
@@ -26,15 +27,11 @@ class InheritancesAPI(Resource):
     @api.expect(get_inheritances_parser, validate=True)
     def get(self):
         args = split_dict_args(request.args)
-        inher_select_a = Inheritances.query
+        inhers = Inheritances.query
         if 'menu_id' in args:
-            inher_select_a = Inheritances.query.filter(Inheritances.menu_id_ancestor.in_(args['menu_id']))
-            inher_select_d = Inheritances.query.filter(Inheritances.menu_id_descendant.in_(args['menu_id'])).filter_by(reversible=True)
-
-        if 'inher_select_d' in globals():
-            res = inher_select_a.filter_by(active=True).all() + inher_select_d.filter_by(active=True).all()
-        else:
-            res = inher_select_a.filter_by(active=True).all()
+            Inheritances.menu_id_ancestor.in_(args['menu_id'])
+            inher_select_a = Inheritances.query.filter(or_(Inheritances.menu_id_ancestor.in_(args['menu_id']), and_(Inheritances.menu_id_descendant.in_(args['menu_id']), Inheritances.reversible == True)))
+        inhers = inhers.filter_by(active=True).all()
         return res, 200
 
     @api.marshal_with(connection_model)
