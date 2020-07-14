a1d9c4ebad1eac9d262771598a0ed6f406aa91e1
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-07-14 23:05:46 +0300

    добавлена замена переменных

--- api_v1/main/endpoints/replace/db.py
+++ api_v1/main/endpoints/replace/db.py
@@ -17,4 +17,4 @@ class Replace(db.Model):
     author = db.Column(db.Integer, nullable=False, default=0)
 
     def __repr__(self):
-        return '<Menu %r>' % self.id
+        return '<Replace %r>' % self.id
