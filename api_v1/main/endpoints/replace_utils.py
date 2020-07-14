2fc3580fa7dd63f958dfbbb745effae425da15cc
Владислав Ковальчук <dobro322@yandex.ru>
2020-07-15 01:06:55 +0300

    Replace fix

--- api_v1/main/endpoints/replace_utils.py
+++ api_v1/main/endpoints/replace_utils.py
@@ -1,3 +1,13 @@
-from functools import wraps
-from flask import request, abort
-import os
+from api_v1.main.endpoints.replace.db import Replace
+
+
+def text_replace(replace_object):
+    replace = Replace.query.all()
+    variables = {}
+    for k in replace:
+        variables[k.name] = k.value
+
+    for i in replace_object:
+        i.text = i.text.format(**variables)
+
+    return replace_object
