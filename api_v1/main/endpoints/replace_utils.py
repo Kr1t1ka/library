510912aea124c675575b639050674c03f7e02d17
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-07-16 01:57:33 +0300

    save коммит

--- api_v1/main/endpoints/replace_utils.py
+++ api_v1/main/endpoints/replace_utils.py
@@ -1,8 +1,17 @@
+import re
+
 from api_v1.main.endpoints.replace.db import Replace
 
 
 def text_replace(replace_object):
+
+    names_arr = changing_array(re.findall('{\w+}', replace_object[0].text))
+
     replace = Replace.query.all()
+    tmp = Replace.query.filter(Replace.name in names_arr).all()
+
+    print(replace[0].name in names_arr)
+
     variables = {}
     for k in replace:
         variables[k.name] = k.value
@@ -11,3 +20,10 @@ def text_replace(replace_object):
         i.text = i.text.format(**variables)
 
     return replace_object
+
+
+def changing_array(array):
+    res = []
+    for i in range(len(array)):
+        res.append(array[i][1:-1])
+    return res
