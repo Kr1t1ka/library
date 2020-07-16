e5ffb119689fb31cdd30017b44f4f24f160ecbda
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-07-16 16:25:38 +0300

    смолл фикс

--- api_v1/main/endpoints/replace_utils.py
+++ api_v1/main/endpoints/replace_utils.py
@@ -1,15 +1,20 @@
 import re
-# from itertools import groupby
+from itertools import groupby
 
 from api_v1.main.endpoints.replace.db import Replace
 
 
 def text_replace(filling_obj):
+    all_text = ''
 
     for obj in filling_obj:
-        names_arr = changing_array(re.findall('{\w+}', obj.text))
-        replace_arr = Replace.query.filter(Replace.name.in_(names_arr)).all()
-        replace_obj = {replace.name: replace.value for replace in replace_arr}
+        all_text += obj.text
+
+    names_arr = [el for el, _ in groupby(changing_array(re.findall('{\w+}', all_text)))]
+    replace_arr = Replace.query.filter(Replace.name.in_(names_arr)).all()
+    replace_obj = {replace.name: replace.value for replace in replace_arr}
+
+    for obj in filling_obj:
         obj.text = obj.text.format(**replace_obj)
 
     return filling_obj
@@ -22,19 +27,12 @@ def changing_array(array):
     return res
 
 
-""""""
-
 """def text_replace(filling_obj):
-    all_text = ''
 
     for obj in filling_obj:
-        all_text += obj.text
-
-    names_arr = [el for el, _ in groupby(changing_array(re.findall('{\w+}', all_text)))]
-    replace_arr = Replace.query.filter(Replace.name.in_(names_arr)).all()
-    replace_obj = {replace.name: replace.value for replace in replace_arr}
-
-    for obj in filling_obj:
+        names_arr = changing_array(re.findall('{\w+}', obj.text))
+        replace_arr = Replace.query.filter(Replace.name.in_(names_arr)).all()
+        replace_obj = {replace.name: replace.value for replace in replace_arr}
         obj.text = obj.text.format(**replace_obj)
 
     return filling_obj"""
