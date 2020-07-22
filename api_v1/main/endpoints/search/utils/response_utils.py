0222c2d033bf0a12ab67dab1a0be737ebd846390
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-07-22 16:21:32 +0300

    интелектуальный поиск v0.3.03

--- api_v1/main/endpoints/search/utils/response_utils.py
+++ api_v1/main/endpoints/search/utils/response_utils.py
@@ -15,10 +15,24 @@ def smart_search(request_arr):
     index_lib = {menu.id: menu.tags.split(' ') for menu in all_menu}
     menu_rating = {menu.id: 0 for menu in all_menu}
 
+    print(index_lib)
+    print(request_arr)
+
+    # for menu in index_lib:
+    #     for word in request_arr:
+    #         if word in index_lib[menu]:
+    #             menu_rating[menu] += 1
+    #         else:
+    #             menu_rating[menu] -= 0.5
+
     for menu in index_lib:
-        for word in request_arr:
-            if word in index_lib[menu]:
+        for tag in index_lib[menu]:
+            if tag in request_arr:
                 menu_rating[menu] += 1
+            else:
+                menu_rating[menu] -= 0.1
+
+    print(menu_rating)
 
     res = [k for k, v in menu_rating.items() if v == max(menu_rating.values())]
     response = Menu.query.filter(Menu.id.in_(res)).all()
