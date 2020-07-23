796aaf0f33b7519c4addf270a6bbbea1df5efac6
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-07-23 15:30:41 +0300

    интелектуальный поиск v2.1

--- api_v1/main/endpoints/search/utils/response_utils.py
+++ api_v1/main/endpoints/search/utils/response_utils.py
@@ -2,7 +2,6 @@ import re
 import operator
 
 from api_v1.main.endpoints.menu.db import Menu
-from api_v1.main.endpoints.search.utils.request_utils import lemmatization
 
 
 def smart_search(request_arr):
@@ -10,10 +9,10 @@ def smart_search(request_arr):
     Получает обработаный запрос пользователя в виде массива, сопостовляет с тегами статей и подбирате наиболее
     подходящую статью.
     :param request_arr:
-    :return: <Menu>
+    :return: [<Menu>,...  ]
     """
     all_menu = Menu.query.all()
-    index_lib = {menu.id: [lemmatization(word) for word in menu.tags.split(' ')] for menu in all_menu}
+    index_lib = {menu.id: menu.tags.split(' ') for menu in all_menu}
     menu_rating = {menu.id: 0 for menu in all_menu}
 
     for menu in index_lib:
