45e9bdd393910bf76cfe438425af67a0c95dd191
Pavel Kukushkin <56510732+Kr1t1ka@users.noreply.github.com>
2020-08-17 19:03:44 +0300

    add Attachment

--- api_v1/main/endpoints/menu/api.py
+++ api_v1/main/endpoints/menu/api.py
@@ -3,6 +3,7 @@ from flask_restx import Namespace, Resource, fields
 
 from api_v1.main import db
 from api_v1.main.endpoints.menu.db import Menu
+from api_v1.main.endpoints.inheritances.api import connection_model
 from api_v1.main.utils import split_dict_args
 from api_v1.main.endpoints.replace.utils.replace_utils import text_replace
 
@@ -14,7 +15,9 @@ menu_model = api.model('Menu', model={'id': fields.Integer(description='The id m
                                       'added': fields.DateTime(description='The date menu', readonly=True),
                                       'active': fields.Boolean(description='activated / deactivated'),
                                       'author_id': fields.Integer(description='author of the text'),
-                                      'tags': fields.String(description='The text menu')})
+                                      'tags': fields.String(description='The text menu'),
+                                      'inheritances': fields.Nested(api.model('connection_model ', connection_model))
+                                      })
 
 menu_parser = api.parser()
 menu_parser.add_argument('menu_ids', required=False, location='args')
--- api_v1/main/endpoints/menu/db.py
+++ api_v1/main/endpoints/menu/db.py
@@ -16,6 +16,23 @@ class Menu(db.Model):
     inheritances = db.relationship('Inheritances',
                                    primaryjoin="or_(Menu.id==Inheritances.menu_id_descendant, Menu.id==Inheritances.menu_id_ancestor)",
                                    lazy='dynamic')
+    attachment = db.relationship('Attachment',
+                                 primaryjoin="Menu.id==Attachment.menu_id",
+                                 lazy='dynamic')
 
     def __repr__(self):
         return '<Menu %r>' % self.id
+
+
+class Attachment(db.Model):
+    __tablename__ = 'attachment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'))
+    vk_active = db.Column(db.Boolean, nullable=False, default=True)
+    telegram_active = db.Column(db.Boolean, nullable=False, default=True)
+    vk_attachment = db.Column(db.String(120), default='photo-128637216_456239172')
+    telegram_attachment = db.Column(db.String(120), default='default_name')
+
+    def __repr__(self):
+        return '<Attachment %r>' % self.id
